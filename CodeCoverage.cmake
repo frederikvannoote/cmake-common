29e0bb354461b20c8467f07973592bb3780078b2
Frederik Vannoote <frederik.vannoote@barco.com>
2017-02-13 20:32:14 +0100

    Restore code coverage

--- CodeCoverage.cmake
+++ CodeCoverage.cmake
@@ -1,34 +1,30 @@
 # Enable Code Coverage
 #
 # USAGE:
-# 1. Copy this file into your cmake modules path.
-#
-# 2. Add the following line to your CMakeLists.txt:
+# 1. Add the following line to your CMakeLists.txt:
 #      INCLUDE(CodeCoverage)
 #
-# 3. Call the function ENABLE_CODE_COVERAGE
-#    This functions sets compiler flags to turn off optimization and enable coverage:
-#    SET(CMAKE_CXX_FLAGS_DEBUG "-g -O0 -fprofile-arcs -ftest-coverage")
+# 2. Build a Debug build:
+#	 Add the following option: -DCODE_COVERAGE=True
 #
-# 4. Build a Debug build:
-#	 cmake -DCMAKE_BUILD_TYPE=Debug ..
-#	 make
-#	 make my_coverage_target
+# 3. Run the application(s) where you want to analyse the coverage
 #
-# Note: code coverage can only be enabled when in debug build!
+# 4. Generate the report by calling one of these targets:
+#    - coverage-html (HTML format)
+#    - coverage-xml (cobertura XML format)
 #
 # Requirements: gcov and gcovr
 
-# Check prereqs
-find_package(Gcov)
-find_package(Gcovr)
-
 function(ENABLE_CODE_COVERAGE)
-    if (GCOV_FOUND AND GCOVR_FOUND)
+    if(NOT CMAKE_COMPILER_IS_GNUCXX AND NOT CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+        message(WARNING "Compiler is not GNU gcc or Clang! Code coverage disabled.")
+    elseif( NOT CMAKE_BUILD_TYPE STREQUAL "Debug" )
+        message(FATAL "Code coverage results with an optimized (non-Debug) build may be misleading. Code coverage will not run." )
+    else()
         set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -g -O0 -fprofile-arcs -ftest-coverage")
         if (CMAKE_COMPILER_IS_GNUCXX AND NOT UNIX)
             link_libraries(debug gcov)
-        endif(WIN32 AND NOT UNIX)
+        endif(CMAKE_COMPILER_IS_GNUCXX AND NOT UNIX)
         if (NOT WIN32)
             set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -fPIC")
         endif (NOT WIN32)
@@ -36,7 +32,35 @@ function(ENABLE_CODE_COVERAGE)
         set(CMAKE_CXX_FLAGS_DEBUG ${CMAKE_CXX_FLAGS_DEBUG} PARENT_SCOPE)
 
         message(STATUS "Code Coverage Enabled")
-    else()
-        message("-- Code Coverage NOT Enabled because gcov and/or gcovr was not found.")
     endif()
 endfunction()
+
+option(CODE_COVERAGE "Code coverage" OFF)
+if(CODE_COVERAGE)
+    find_package(Gcov REQUIRED)
+    find_package(Lcov REQUIRED)
+
+    enable_code_coverage()
+
+    file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/coverage)
+
+    add_custom_target("coverage-report")
+    add_custom_command(TARGET "coverage-report"
+        COMMAND ${LCOV_EXECUTABLE} --quiet --capture --directory . --base-directory ${CMAKE_SOURCE_DIR} --no-external -o coverage.info
+        COMMAND ${LCOV_EXECUTABLE} --quiet --remove coverage.info \*moc_\* -o coverage.info
+        COMMAND ${LCOV_EXECUTABLE} --quiet --remove coverage.info \*.moc -o coverage.info
+        COMMAND ${LCOV_EXECUTABLE} --quiet --remove coverage.info \*tst_\* -o coverage.info
+        COMMAND ${LCOV_EXECUTABLE} --list coverage.info
+        COMMAND ${LCOV_EXECUTABLE} --summary coverage.info
+        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+        COMMENT "Create code coverage report"
+        VERBATIM)
+
+    add_custom_target("coverage-html" DEPENDS "coverage-report")
+    add_custom_command(TARGET "coverage-html"
+        COMMAND ${GENHTML_EXECUTABLE} coverage.info --show-details -o coverage
+        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+        COMMENT "Create code coverage html report"
+        VERBATIM)
+
+endif(CODE_COVERAGE)
