a10ab52a2563339233fbb73e29c6fd284aa2aa1f
frederik vannoote <frederik.vannoote@barco.com>
2017-05-30 14:16:53 +0200

    Create package from installed files.

--- Package.cmake
+++ Package.cmake
@@ -1,8 +1,8 @@
 # Create an SDK package containing all installed files.
 
 add_custom_target(sdk
-    ${CMAKE_COMMAND} --build ${CMAKE_BINARY_DIR} --target install DESTDIR=${CMAKE_BINARY_DIR}/sdk
-    COMMAND ${CMAKE_COMMAND} -E tar "cfvz" "sdk.tar.gz" "${CMAKE_INSTALL_PREFIX}"
+    COMMAND ${CMAKE_COMMAND} -E chdir ${CMAKE_INSTALL_PREFIX} ${CMAKE_COMMAND} -E tar "cfz" "${CMAKE_BINARY_DIR}/sdk.tar.gz" --files-from=${CMAKE_BINARY_DIR}/install_manifest.txt
     WORKING_DIRECTORY "${CMAKE_BINARY_DIR}"
-    COMMENT "Packaging artefacts..."
+    COMMENT "Packaging artefacts into ${CMAKE_BINARY_DIR}/sdk.tar.gz ..."
     VERBATIM)
+add_dependencies(sdk install)
