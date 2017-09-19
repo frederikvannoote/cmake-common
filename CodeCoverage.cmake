3fd3e38f55a87cd42d55c2d887c7437559849bc2
Frederik Vannoote <frederik.vannoote@barco.com>
2017-09-19 16:37:20 +0200

    Exclude tests from catch framework.

--- CodeCoverage.cmake
+++ CodeCoverage.cmake
@@ -50,9 +50,11 @@ if(CODE_COVERAGE)
         COMMAND ${LCOV_EXECUTABLE} --quiet --remove coverage.info \*moc_\* -o coverage.info
         COMMAND ${LCOV_EXECUTABLE} --quiet --remove coverage.info \*.moc -o coverage.info
         COMMAND ${LCOV_EXECUTABLE} --quiet --remove coverage.info \*tst_\* -o coverage.info
+        COMMAND ${LCOV_EXECUTABLE} --quiet --remove coverage.info \*test_\* -o coverage.info
         COMMAND ${LCOV_EXECUTABLE} --quiet --remove coverage.info \*tests\* -o coverage.info
         COMMAND ${LCOV_EXECUTABLE} --quiet --remove coverage.info \*qrc_\* -o coverage.info
         COMMAND ${LCOV_EXECUTABLE} --quiet --remove coverage.info ui_\*.h -o coverage.info
+        COMMAND ${LCOV_EXECUTABLE} --quiet --remove coverage.info catch.hpp -o coverage.info
         COMMAND ${LCOV_EXECUTABLE} --list coverage.info
         COMMAND ${LCOV_EXECUTABLE} --summary coverage.info
         WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
